5155dea7c2a42db348e47fdf1179541b3d29c070
rahulpalamut <rahulpalamut@yahoo.com>
2013-09-17 00:51:01 -0700

    Added gitlab repo

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ custom Configuration/initialization files I use for various tools
 
 Sat Sep 14 22:12:17 PDT 2013
     - my emacs init.el file
+Tue Sep 17 00:50:21 PDT 2013
+    - added gitlab repo to config file
--- init.el
+++ init.el
@@ -1,6 +1,20 @@
 ;; Rahul Palamuttam
 ;; use C-q C-# to enter a ctrl- command where # is the key u wish to add
 
+;;---------------------------------Package Archives ------------------------------------------------
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;;---------------------------------Preferences-------------------------------------------------------
+
+;; line numbers
+(global-linum-mode t)
+
+;;enable smartparens
+;;-------------------------------------Macros--------------------------------------------------------
+(package-initialize)
+(smartparens-global-mode t)
 ;; Makefile macro
 ;; Description: runs the make command and pipes the output to a new buffer called makefile
 ;; Process:
@@ -9,7 +23,5 @@
 ;; Quick-Key: C-c C-m
 
 (fset 'makefile
-(lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("13obmakefile!make30{o" 0 "%d")) arg)))
-
-(define-key global-map [()] 'makefile)
+(lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("13obmakefile!make25{o" 0 "%d")) arg)))
 (global-set-key (kbd "C-c C-m") 'makefile)
